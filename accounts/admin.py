04ffe11078f9ea71363a83f7cfa5035585f98c4b
OgabekMurodullaev <148078268+OgabekMurodullaev@users.noreply.github.com>
2024-11-05 11:28:41 +0500

    models in accounts: created

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from accounts.models import *
+from django.contrib.auth.models import Group
+
+
+admin.site.unregister(Group)
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'email', 'phone_number')
+
+
+@admin.register(VerificationOtp)
+class VerificationOtpAdmin(admin.ModelAdmin):
+    list_display = ["id", "user", "code", "expires_in"]
+
+
+@admin.register(UserAddress)
+class UserAddressAdmin(admin.ModelAdmin):
+    list_display = ["id", "user", "street"]
+
